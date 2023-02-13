9155359779645824dd3c05a06ec99438931d1ff8
Sangmin Lee <tkdals2317@naver.com>
2023-02-13 23:49:08 +0900

    item 19 예제 코드

---
+++ src/main/java/org/example/item19/Sub.java
@@ -0,0 +1,18 @@
+package org.example.item19;
+
+import java.time.Instant;
+
+public class Sub extends Super{
+    // 초기화되지 않은 final 필드. 생성자에서 초기화한다.
+    private final Instant instant;
+
+    Sub() {
+        instant = Instant.now();
+    }
+
+    @Override
+    public void overrideMe() {
+        System.out.println(instant);
+    }
+
+}
