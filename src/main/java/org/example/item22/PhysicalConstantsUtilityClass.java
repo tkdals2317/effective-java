70f202b48b058961690828aa2174a1b7cc84d554
Sangmin Lee <tkdals2317@naver.com>
2023-02-15 00:49:41 +0900

    :sparkles: item 22 예제 코드

---
+++ src/main/java/org/example/item22/PhysicalConstantsUtilityClass.java
@@ -0,0 +1,15 @@
+package org.example.item22;
+
+public class PhysicalConstantsUtilityClass {
+    // 인스턴스화 방지를 위한 private 생성자
+    private PhysicalConstantsUtilityClass() {};
+
+    // 아보가드로 수 (1/몰)
+    public final double AVOGADROS_NUMBER = 6.022_140_857e23;
+
+    // 볼츠만 상수 (J/K)
+    public final double BOLZMANN_CONSTANT = 1.380_648_52e-23;
+
+    // 전자 질량 (Kg)
+    public final double ELECTRON_MASS = 9.109_383_56e-31;
+}
