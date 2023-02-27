b7228618b8a226d23648fa60cbd6980c28e70e98
Sangmin Lee <tkdals2317@naver.com>
2023-02-27 10:29:45 +0900

    :memo: item 28 - 예제 소스 Chooser 제네릭 적용 후 업로드

--- src/main/java/org/example/item28/Chooser.java
+++ src/main/java/org/example/item28/Chooser.java
@@ -4,12 +4,12 @@ import java.util.Collection;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Chooser {
-    private final Object[] choiceArray;
+public class Chooser<T> {
+    private final T[] choiceArray;
 
 
-    public Chooser(Collection choiceArray) {
-        this.choiceArray = choiceArray.toArray();
+    public Chooser(Collection<T> choiceArray) {
+        this.choiceArray = (T[]) choiceArray.toArray();
     }
 
     //반환된 Object 타입을 형변환 하여 사용해야 한다.
