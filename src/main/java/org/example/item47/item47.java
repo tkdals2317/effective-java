18412fc16c3aa614a2151480a32620bbf5275fe1
lsm0506 <lsm0506@midasin.com>
2023-03-29 18:45:46 +0900

    :sparkles: item 47 예제 소스 코드 업로드

---
+++ src/main/java/org/example/item47/item47.java
@@ -0,0 +1,31 @@
+package org.example.item47;
+
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class item47 {
+    public static void main(String[] args) {
+        // 코드 47-1 자바 타입 추론의 한계로 컴파일 되지 않는다.
+        for (ProcessHandle ph : ProcessHandle.allProcesses()::iterator) {
+            // 프로세스를 처리한다.
+        }
+        // 코드 47-2 스트림 반복하기 위한 '끔직한' 우회 방법
+        for (ProcessHandle ph : (Iterable<ProcessHandle>) ProcessHandle.allProcesses()::iterator) {
+            // 프로세스를 처리한다.
+        }
+
+        for (ProcessHandle ph : iterableOf(ProcessHandle.allProcesses())) {
+
+        }
+    }
+
+    // 코드 47-3 Stream<E>를 Iterable<E>로 중개해주는 어댑터
+    private static <E> Iterable<E> iterableOf(Stream<E> stream) {
+        return stream::iterator;
+    }
+
+    // 코드 47-4 Iterable<E>를 Stream<E>로 중개해주는 어댑터
+    private static <E> Stream<E> streamOf(Iterable<E> iterable) {
+        return StreamSupport.stream(iterable.spliterator(), false);
+    }
+}
