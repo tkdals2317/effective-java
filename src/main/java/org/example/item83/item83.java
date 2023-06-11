436c64e0b63d2d13b4a883680e05ce7918529ae9
lsm0506 <tkdals2317@gmail.com>
2023-06-11 17:26:55 +0900

    :sparkles: item 83 소스코드 업로드

--- src/main/java/org/example/item83/item83.java
+++ src/main/java/org/example/item83/item83.java
@@ -16,13 +16,47 @@ public class item83 {
         if (field == null)
             field = computeFieldValue();
         return field;
-    }*/
+    }
+    */
 
+    /*
     // 코드 83-3 정적 필드용 지연 초기화 홀더 클래스 관용구
     private static class FiledHolder {
         static final FieldType field = computeFieldValue();
     }
 
     private static FieldType getField() {return FiledHolder.field;}
+    */
 
+    /*
+    // 코드 83-4 인스턴스 필드 지연 초기화용 이중검사 관용구
+    private volatile FieldType field;
+
+    private FieldType getField() {
+        FieldType result = field;
+        if (result != null) { // 첫 번째 검사 (락 사용 안함)
+            return result;
+        }
+
+        synchronized (this) {
+            if (field == null) { // 두 번째 검사 (락 사용)
+                field = computeFieldValue();
+            }
+            return field;
+        }
+    }
+    */
+
+
+    // 코드 83-5 단일검사 관용구 - 초기화가 중복해서 일어날 수 있다.
+    private volatile FieldType field;
+
+    private FieldType getField() {
+        FieldType result = field;
+        if (result == null) {
+            field = result = computeFieldValue();
+        }
+        return field;
+    }
 }
+
